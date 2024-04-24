c56995b84000a4a79a2bd42daab4b1bd84f42c09
Dan Politica <73758508+benochi@users.noreply.github.com>
2024-04-24 14:33:59 -0500

    add really sick POST request toNEXT route

--- src/app/comments/data.ts
+++ src/app/comments/data.ts
@@ -1,4 +1,4 @@
-const comments = [
+export const comments = [
   { id: 1, text: "This is the first comment." },
   { id: 2, text: "Here's the second comment." },
   { id: 3, text: "And this is the third comment." }
