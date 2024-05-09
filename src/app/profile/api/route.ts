af523356a15549578d73c23a1df5a002ed937d89
Dan Politica <73758508+benochi@users.noreply.github.com>
2024-05-09 10:28:57 -0500

    Cookies!

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,5 +1,5 @@
 import { type NextRequest } from "next/server";
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 
 export async function GET(request: NextRequest) {
   // const requestHeaders = new Headers(request.headers)
@@ -7,9 +7,17 @@ export async function GET(request: NextRequest) {
   const headersList = headers()
   console.log(headersList.get("Authorization"))
 
+  // const theme = request.cookies.get("theme")
+  // console.log(theme)
+
+  cookies().set("ResultsPerPage", "20")
+  console.log(cookies().get("ResultsPerPage"))
+  console.log(cookies().has("ResultsPerPage"))
+
   return new Response("<h1>Profile API DATA</h1>",{ 
     headers: {
-      "Content-Type": "text/html"
+      "Content-Type": "text/html",
+      "Set-Cookie": "theme=dark"
     }
   });
 }
