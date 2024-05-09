e1b1d011edfc1f19b8f3d8a0c016b30aa41cf0ad
Dan Politica <73758508+benochi@users.noreply.github.com>
2024-05-09 10:05:20 -0500

    set response header

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -2,10 +2,15 @@ import { type NextRequest } from "next/server";
 import { headers } from "next/headers";
 
 export async function GET(request: NextRequest) {
-  const requestHeaders = new Headers(request.headers)
+  // const requestHeaders = new Headers(request.headers)
+   // console.log(requestHeaders.get("Authorization"))
   const headersList = headers()
-  console.log(requestHeaders.get("Authorization"))
   console.log(headersList.get("Authorization"))
-  return new Response("Profile API DATA");
+
+  return new Response("<h1>Profile API DATA</h1>",{ 
+    headers: {
+      "Content-Type": "text/html"
+    }
+  });
 }
 
