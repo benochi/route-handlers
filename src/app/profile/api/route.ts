8ca8c7529388540bc00e20b89d87b79062a42cf6
Dan Politica <73758508+benochi@users.noreply.github.com>
2024-05-09 09:57:42 -0500

    use next Headers, much better.

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,8 +1,11 @@
 import { type NextRequest } from "next/server";
+import { headers } from "next/headers";
 
 export async function GET(request: NextRequest) {
   const requestHeaders = new Headers(request.headers)
+  const headersList = headers()
   console.log(requestHeaders.get("Authorization"))
+  console.log(headersList.get("Authorization"))
   return new Response("Profile API DATA");
 }
 
