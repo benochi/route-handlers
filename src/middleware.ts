89f07fbc50f433bf4e806bfe8cc61d998605d943
Dan Politica <73758508+benochi@users.noreply.github.com>
2024-05-09 10:57:47 -0500

    middleware handlers added.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,24 @@
 //set up middleware in src dir
 import { NextResponse, type NextRequest } from "next/server"
+import { headers, cookies } from "next/headers";
 
 export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL("/", request.url))
+  const response = NextResponse.next()
+
+  const userTheme = request.cookies.get("theme")
+  if(!userTheme) {
+    response.cookies.set("theme", "dark");
+  }
+
+  response.headers.set("customer-header", "custom Value")
+
+  return response;
+  // if (request.nextUrl.pathname === "/profile") {
+  //   return NextResponse.rewrite(new URL("/hello", request.url)) //rewrite redirects without changing url. 
+  // }
+  // return NextResponse.redirect(new URL("/", request.url))
 }
+
 
 //this will match and redirect using matcher approach. 
 // export const config = {
