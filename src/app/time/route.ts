eed4273dd33f63217212328eda6b06c8ff669c42
Dan Politica <73758508+benochi@users.noreply.github.com>
2024-05-09 10:36:26 -0500

    caching avoidance updated.

--- src/app/time/route.ts
+++ src/app/time/route.ts
@@ -1,6 +1,6 @@
-// Opt out of caching
+// Opt out of caching 
 export const dynamic = "force-dynamic"
-
+//can also opt out employing dynamic functions like headers()/cookies() or methods other than GET
 export async function GET(){
   //if using npm run build / npm run start it will CACHE by default, so segment section
   return Response.json({
