387546408790df4f221e25c31dc2c65bb043bc55
Dan Politica <73758508+benochi@users.noreply.github.com>
2024-05-09 10:35:30 -0500

    caching opt out example

--- src/app/time/route.ts
+++ src/app/time/route.ts
@@ -1,3 +1,6 @@
+// Opt out of caching
+export const dynamic = "force-dynamic"
+
 export async function GET(){
   //if using npm run build / npm run start it will CACHE by default, so segment section
   return Response.json({
